d7682abcfecd763fb8c764ae79a33d832c02d84b
weixiao2925 <weixiao2925@gmail.com>
2024-04-21 20:50:26 +0800

    实现菜单伸缩和动态tab（还没研究透）

--- src/views/index/IndexCommon.vue
+++ src/views/index/IndexCommon.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import {reactive, ref} from "vue";
+import {reactive, ref, watch} from "vue";
 import {House, Location, MoreFilled, Notification, Operation} from "@element-plus/icons-vue";
 
 const data=reactive({
@@ -9,6 +9,7 @@ const data=reactive({
 
 //默认状态（true为伸展）
 const isCollapse=ref(true)
+const index_c=ref('1')
 const handleOpen=(key,keyPath)=>{
   console.log(key,keyPath)
 }
@@ -19,6 +20,43 @@ const handleClose=(key,keyPath)=>{
 const toggleCollapse = () => {
   isCollapse.value = !isCollapse.value;
 };
+
+//-----tab测试
+const tabs = ref([]); // 存储打开的Tabs
+const activeTab = ref(''); // 当前激活的Tab
+
+// ...其他代码保持不变
+
+// 打开或激活一个Tab
+const openTab = (index, title) => {
+  let tab = tabs.value.find(t => t.index === index);
+  if (!tab) {
+    tab = { index, title };
+    tabs.value.push(tab);
+  }
+  activeTab.value = index;
+};
+
+// 关闭一个Tab
+const closeTab = (tabIndex) => {
+  const index = tabs.value.findIndex(t => t.index === tabIndex);
+  if (index !== -1) {
+    tabs.value.splice(index, 1);
+
+    // 如果关闭的是当前激活的Tab，并且还有其它Tab打开，激活最后一个Tab
+    if (activeTab.value === tabIndex && tabs.value.length) {
+      activeTab.value = tabs.value[tabs.value.length - 1].index;
+    }
+  }
+};
+// 添加激活对应菜单的逻辑
+const activateMenu = (index) => {
+  index_c.value = index;
+};
+
+watch(activeTab, (newVal) => {
+  activateMenu(newVal);
+});
 </script>
 
 <template>
@@ -27,25 +65,25 @@ const toggleCollapse = () => {
       <div style="float:left;">
         <div v-if="!isCollapse" style="padding: 10px 10px 10px 20px;">xxx管理系统</div>
         <el-menu
-            default-active="1"
+            :default-active="index_c"
             :collapse="isCollapse"
             class="el-menu-vertical-demo"
             @open="handleOpen"
             @close="handleClose"
         >
-          <el-menu-item index="1">
+          <el-menu-item index="1" @click="openTab('1', '系统首页')">
             <el-icon size="15px"><House /></el-icon>
             <template #title>系统首页</template>
           </el-menu-item>
-          <el-menu-item index="2">
+          <el-menu-item index="2" @click="openTab('2', '用户管理')">
             <el-icon size="15px"><Notification /></el-icon>
             <template #title>用户管理</template>
           </el-menu-item>
-          <el-menu-item index="3">
+          <el-menu-item index="3" @click="openTab('3', 'xx管理')">
             <el-icon size="15px"><Notification /></el-icon>
             <template #title>xx管理</template>
           </el-menu-item>
-          <el-menu-item index="3">
+          <el-menu-item index="4" @click="openTab('4', 'xx管理')">
             <el-icon size="15px"><Notification /></el-icon>
             <template #title>xx管理</template>
           </el-menu-item>
@@ -53,10 +91,22 @@ const toggleCollapse = () => {
       </div>
       <div style="float:left;">
         <el-radio-group v-model="isCollapse">
-          <el-button @click="toggleCollapse">
-            <el-icon><Operation /></el-icon>
+          <el-button @click="toggleCollapse" style="height: 2rem" plain >
+            <el-icon ><Operation /></el-icon>
           </el-button>
         </el-radio-group>
+      </div>
+      <!-- 顶部Tab显示区域 -->
+      <div id="tabs-container" style="float:left;">
+        <el-tabs v-model="activeTab" type="card" closable @tab-remove="closeTab" @update:modelValue="activateMenu">
+          <el-tab-pane
+              v-for="tab in tabs"
+              :key="tab.index"
+              :label="tab.title"
+              :name="tab.index"
+          >
+          </el-tab-pane>
+        </el-tabs>
       </div>
     </div>
   </div>
@@ -64,12 +114,14 @@ const toggleCollapse = () => {
     <div style="float: right">
       <el-dropdown  type="primary">
         <div>
-          <div style="float:left;padding: 10px 0 0 0;">
+          <div style="float:left;padding: 1px 0 0 0;">
             <el-avatar :size="'default'" style=""/>
           </div>
-          <div style="font-size: 1.2rem;float: right;padding: 15px ;">
-            {{data.test}}
-          </div>
+          <transition name="el-fade-in">
+            <div style="font-size: 1rem;float: right;padding: 10px ;">
+              {{data.test}}
+            </div>
+          </transition>
         </div>
         <template #dropdown>
           <el-dropdown-menu>
@@ -86,10 +138,11 @@ const toggleCollapse = () => {
 <style scoped>
   #menu_top{
     width: auto;
-    height: 3rem;
+    height: 2.1rem;
     background-color: #11ff00;
   }
   .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
+    width: 150px;
+    height: 100%;
   }
 </style>
