755b6d85f4ba6a5896962c6029def331001dca3e
weixiao2925 <weixiao2925@gmail.com>
2024-04-22 20:03:14 +0800

    把index的公共组件再分家了，圆满删除之前的总组件

--- src/views/indexPage.vue
+++ src/views/indexPage.vue
@@ -1,12 +1,11 @@
 <script setup>
 
-import IndexCommon from "@/views/index/IndexCommon.vue";
 import IndexCommonTop from "@/views/index/indexcommon/IndexCommonTop.vue";
 import IndexCommonLeft from "@/views/index/indexcommon/IndexCommonLeft.vue";
 </script>
 
 <template>
-<!--  <IndexCommon/>-->
+
   <IndexCommonLeft/>
   <IndexCommonTop/>
   <router-view/>
