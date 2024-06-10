74915f9e2f944e3808a6bc72278b961359df407e
weixiao2925 <weixiao2925@gmail.com>
2024-06-10 23:09:54 +0800

    实现歌单管理页的更换照片（有提交后不能清空的bug）

--- src/views/index/IndexPlaylistManagement.vue
+++ src/views/index/IndexPlaylistManagement.vue
@@ -1,11 +1,11 @@
 <script setup>
 
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 import testImage from '@/assets/test.jpg'
 import {Search} from "@element-plus/icons-vue";
 import {useRouter} from "vue-router";
-import {fetchAndDisplayFile, get} from "@/net/index.js";
-import {ElMessage, ElMessageBox} from "element-plus";
+import {fetchAndDisplayFile, get, uploadFile} from "@/net/index.js";
+import {ElMessage, ElMessageBox, genFileId} from "element-plus";
 import {singerInfoStore} from "@/store/singer.js";
 import {playlistInfoStore} from "@/store/playlist.js";
 import IndexSingerEditingPage from "@/views/index/indexSingerManagement/IndexSingerEditingPage.vue";
@@ -156,7 +156,42 @@ function editingSinger(row) {
   playlist_id.value = row.row.playlist_id;
   dialogFormVisible_change.value=!dialogFormVisible_change.value;
 }
+//----文件上传
+const form=reactive({
+  aFile:null
+})
+const uploadRef=ref(null)
+//获取文件（mp3）
+const getFileUpload = (file) => {
+  form.aFile = file; // 将文件对象保存到状态中
+  // console.log(form.mp3File );
+};
 
+//替换文件（）
+const handleExceed = (files) => {
+  uploadRef.value.clearFiles()
+  const file = files[0]
+  file.uid = genFileId()
+  uploadRef.value.handleStart(file)
+}
+//上传文件()
+const uploadFile_Button = (row) => {
+  // console.log(row.row)
+  // 构建请求数据
+  // console.log(row.row.song_id)
+  uploadRef.value.clearFiles()
+  const formData = new FormData();
+  if (form.aFile !=null){
+    formData.append('file', form.aFile.raw);
+    formData.append('playlist_id', row.row.playlist_id); // 这里需要传递正确的 song_id
+    uploadFile('/api/index/upPlaylistAvatar', formData, () => {
+      form.aFile=null
+      // uploadRef.value= null
+      ElMessage.success("上传图片文件成功")
+    });
+  }
+  getData()
+};
 </script>
 
 <template>
@@ -204,7 +239,18 @@ function editingSinger(row) {
       <el-table-column label="歌单照片"  align="center" min-width="50">
         <template #default="row">
           <el-avatar shape="square" :size="80" :src="row.row.playList_path" />
-          <el-button size="small">更新图片</el-button>
+          <el-upload
+              :auto-upload="false"
+              :limit="1"
+              ref="uploadRef"
+              :on-change="getFileUpload"
+              :on-exceed="handleExceed"
+          >
+            <template #trigger>
+              <el-button size="small">更新图片</el-button>
+            </template>
+          </el-upload>
+          <el-button size="small" type="warning" @click="uploadFile_Button(row)">确认更新</el-button>
         </template>
       </el-table-column>
       <el-table-column label="标题" property="playlist_name" align="center" min-width="40"/>
